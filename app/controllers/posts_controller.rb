626ef62b59b6ec8daf3d78ef2806a8ea7fd844f9
Stefan Huynh <huynh.stefan@gmail.com>
2014-08-06 10:37:12 -0400

    Fix inverse association bug between Post and Link

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,9 @@
 class PostsController < ApplicationController
-  
+
   def create
     @post = current_user.posts.build(post_params)
     @post.links.build(link_params)
-    
+
     if @post.save
       redirect_to post_url(@post)
     else
@@ -11,28 +11,28 @@ class PostsController < ApplicationController
       render :new
     end
   end
-  
+
   def new
     @post = Post.new
     render :new
   end
-  
+
   def show
     @post = Post.find(params[:id])
     render :show
   end
 
   private
-  
+
   def post_params
     params.require(:post).permit(:title, :body, share_ids: [])
   end
-  
-  def link_params    
-    params.permit(link: :url)
-      .require(:link)
+
+  def link_params
+    params.permit(links: :url)
+      .require(:links)
       .values
       .reject { |hash| hash.values.all?(&:blank?) }
   end
-  
+
 end
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,12 +1,11 @@
 class Link < ActiveRecord::Base
   belongs_to(
     :post,
-    class_name: "Link",
+    class_name: "Post",
     foreign_key: :post_id,
     primary_key: :id,
     inverse_of: :links
   )
-  
-  validates :post_id, :url, presence: true
-  validates :post_id, numericality: true
+
+  validates :post, :url, presence: true
 end
